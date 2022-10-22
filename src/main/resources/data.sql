86f7cb71a0459669a4e2f2ae822897b9eb76174c
Dario Mauri <dario.mauri9@gmail.com>
2022-10-22 15:19:16 +0200

    Add data initialization to h2 db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO ICHIRO.DOG
+(DOG_ID, DOG_NAME, DOG_GENDER, DOG_COUNTRY, DOG_DATE_OF_BIRTH, DOG_COLOR, DOG_IMAGE_URL, DOG_PROFILE_URL, DOG_GENERATION, DOG_MOTHER_ID, DOG_FATHER_ID)
+VALUES
+(2, 'mother', 'Male', 'Russia', TO_DATE('2021-10-14', 'yyyy-mm-dd'), 'Red', 'https://ilmegliosito.altervista.org/ichiro/IMG_8417.jpg', 'http://www.shiba-pedigree.ru/details.php?id=79238', 2, null, null);
+INSERT INTO ICHIRO.DOG
+(DOG_ID, DOG_NAME, DOG_GENDER, DOG_COUNTRY, DOG_DATE_OF_BIRTH, DOG_COLOR, DOG_IMAGE_URL, DOG_PROFILE_URL, DOG_GENERATION, DOG_MOTHER_ID, DOG_FATHER_ID)
+VALUES
+(3, 'father', 'Male', 'Russia', TO_DATE('2021-10-14', 'yyyy-mm-dd'), 'Red', 'https://ilmegliosito.altervista.org/ichiro/IMG_8417.jpg', 'http://www.shiba-pedigree.ru/details.php?id=79238', 2, null, null);
+INSERT INTO ICHIRO.DOG
+(DOG_ID, DOG_NAME, DOG_GENDER, DOG_COUNTRY, DOG_DATE_OF_BIRTH, DOG_COLOR, DOG_IMAGE_URL, DOG_PROFILE_URL, DOG_GENERATION, DOG_MOTHER_ID, DOG_FATHER_ID)
+VALUES
+(1, 'Ichiro Go Takimisou', 'Male', 'Russia', TO_DATE('2021-10-14', 'yyyy-mm-dd'), 'Red', 'https://ilmegliosito.altervista.org/ichiro/IMG_8417.jpg', 'http://www.shiba-pedigree.ru/details.php?id=79238', 1, 2, 3);
